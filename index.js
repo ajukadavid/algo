1e28ab941bbe01213490abea75316b5262728293
Ajuka David <ajuka.zephiniah@gmail.com>
2022-10-12 11:57:41 +0000

    added howSum code

--- index.js
+++ index.js
@@ -38,4 +38,41 @@
 
 // console.log(factorial(3))
 
-const canSum = (targetSum, numbers) => {}
+//CanSum: check if combination of numbers in array can supup to target number without remainder
+
+// const canSum = (targetSum, numbers, memo = {}) => {
+//   if(targetSum in memo) return memo[targetSum]
+//   if(targetSum === 0) return true
+//   if(targetSum < 0) return false
+  
+//   for(let num of numbers){
+//     const remainder = targetSum - num
+//     if(canSum(remainder, numbers) === true){
+//       memo[targetSum] = true
+//       return true
+//     }
+//   }
+//   memo[targetSum] = false
+//   return false
+    
+// }
+
+// console.log(canSum(7, [3, 4]))
+
+//howSum: similar to canSum but r eturns the array possible number of combinations pr return null if no combination in array can match targetsum
+
+const howSum = (targetSum, numbers) => {
+  if (targetSum === 0) return []
+  if(targetSum < 0) return null
+
+  for(let num of numbers) {
+    const remainder = targetSum - num;
+   const remainderResult =  howSum(remainder, numbers)
+    if(remainderResult !== null) {
+      return [...remainderResult, num]
+    }
+  }
+  return null
+}
+
+console.log(howSum(7, [2, 3]))
